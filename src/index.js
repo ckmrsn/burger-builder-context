17368e4f463ccba6a6137c8f5e31767bbf1e832d
Ersin <ckmrsn@gmail.com>
2021-04-18 01:38:48 +0300

    patika homework completed

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import HamburgerContextProvider from "./context/HamburgerContext";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import HamburgerContextProvider from './context/HamburgerContext'
 
 ReactDOM.render(
-  <React.StrictMode>
-      {/* Uygulamayı context provider ile sarmalayınız */}
-      <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+	<React.StrictMode>
+		{/* Uygulamayı context provider ile sarmalayınız */}
+		<HamburgerContextProvider>
+			<App />
+		</HamburgerContextProvider>
+	</React.StrictMode>,
+	document.getElementById('root')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
